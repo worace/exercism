9931e75b4c7c1f123d4e7abf0bdd018f33a3421e
Horace Williams <horace.d.williams@gmail.com>
2016-03-24 19:04:21 -0600

    anagram first pass

--- clojure/anagram/src/anagram.clj
+++ clojure/anagram/src/anagram.clj
@@ -1,4 +1,10 @@
-(ns anagram)
+(ns anagram
+  (:require [clojure.string :refer [lower-case]]))
 
-(anagrams-for [str]
-              [])
+(defn anagrams-for
+  [word possibilities]
+  (filter (fn [possibility]
+            (and (= (sort (lower-case possibility))
+                    (sort (lower-case word)))
+                 (not (= (lower-case word) (lower-case possibility)))))
+          possibilities))
