05a88af3a5cfea6978840aabb8fbee1dbc6e70b6
Horace Williams <horace.d.williams@gmail.com>
2016-03-24 19:13:28 -0600

    tidy up anagram

--- clojure/anagram/src/anagram.clj
+++ clojure/anagram/src/anagram.clj
@@ -1,10 +1,16 @@
 (ns anagram
   (:require [clojure.string :refer [lower-case]]))
 
+(defn same-word? [word possibility]
+  (= (lower-case word) (lower-case possibility)))
+
+(def character-pool (comp sort lower-case))
+
+(defn anagram? [word possibility]
+  (and (= (character-pool possibility)
+          (character-pool word))
+       (not (same-word? word possibility))))
+
 (defn anagrams-for
   [word possibilities]
-  (filter (fn [possibility]
-            (and (= (sort (lower-case possibility))
-                    (sort (lower-case word)))
-                 (not (= (lower-case word) (lower-case possibility)))))
-          possibilities))
+  (filter (partial anagram? word) possibilities))
