6055f87919f3ec523fefcc33fc77638109ef0d80
Horace Williams <horace.d.williams@gmail.com>
2016-06-23 10:15:00 -0500

    octal exercism

---
+++ clojure/octal/src/octal.clj
@@ -0,0 +1,19 @@
+(ns octal)
+
+(defn char-value-mapping [base]
+  (zipmap (apply str (range base)) (range base)))
+
+(defn -to-decimal [base numstring]
+  (let [mapping (char-value-mapping base)]
+    (if-not (every? mapping numstring)
+      0
+      (loop [position 1
+             num 0
+             digits (reverse numstring)]
+        (if (empty? digits)
+          num
+          (recur (* base position)
+                 (+ num (* (mapping (first digits)) position))
+                 (rest digits)))))))
+
+(def to-decimal (partial -to-decimal 8))
