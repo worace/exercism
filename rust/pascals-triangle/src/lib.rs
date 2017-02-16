a15565cb4c29bcbbf28d22bedfc74f70818ae636
Horace Williams <horace.d.williams@gmail.com>
2017-02-15 20:29:27 -0800

    rust pascals triangle

---
+++ rust/pascals-triangle/src/lib.rs
@@ -0,0 +1,52 @@
+//     1
+//    1 1
+//   1 2 1
+//  1 3 3 1
+// 1 4 6 4 1
+
+//     1
+//     1 1
+//     1 2 1
+//     1 3 3 1
+//     1 4 6 4 1
+
+pub struct PascalsTriangle {
+    row_count: u32
+}
+
+
+fn next_row(prev_row: &Vec<u32>) -> Vec<u32> {
+    let mut next_row = Vec::new();
+    for i in 0..prev_row.len() + 1 {
+        let mut value = 0;
+        if i > 0 {
+            match prev_row.get(i - 1) {
+                Some(x) => {value += *x},
+                None => {}
+            }
+        }
+        match prev_row.get(i) {
+            Some(x) => {value += *x},
+            None => {}
+        }
+        next_row.push(value);
+    }
+    next_row
+}
+
+impl PascalsTriangle {
+    pub fn new(row_count: u32) -> Self {
+        PascalsTriangle { row_count: row_count }
+    }
+
+    pub fn rows(&self) -> Vec<Vec<u32>> {
+        let mut prev_row = vec![1];
+        let mut rows = Vec::new();
+        for _ in 0..self.row_count {
+            let next = next_row(&prev_row);
+            rows.push(prev_row);
+            prev_row = next;
+        }
+        rows
+    }
+}
