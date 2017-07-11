29c107d3af0eb12e256f2316c61a923e436fe249
Horace Williams <horace.d.williams@gmail.com>
2017-07-10 19:04:39 -0700

    remove unneeded include

--- cpp/leap/leap.h
+++ cpp/leap/leap.h
@@ -1,12 +1,9 @@
 #if !defined(HELLO_WORLD_H)
 #define HELLO_WORLD_H
 
-#include <string>
-
 namespace leap {
   bool is_leap_year(int year) {
     return year % 400 == 0 || (year % 4 == 0 && year % 100 != 0);
   }
 }
-
 #endif
